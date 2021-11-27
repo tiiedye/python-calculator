d61b704f0583b66fa3581e766707ed3b976a1e14
tiiedye <tiiedye@gmail.com>
2021-11-27 08:18:20 -0800

    adding add, subract, multiply, divide functions

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 # Calculator
+from art import logo
 
+
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+print(logo)
