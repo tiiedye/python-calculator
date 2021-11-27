34ca3da0abcc1b49aaa7ae321f87e0859c2a06b9
tiiedye <tiiedye@gmail.com>
2021-11-27 09:03:17 -0800

    creating a recursive function to add more repeatablity, and for allowing to start over calculations

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Calculator
 from art import logo
+
 print(logo)
 
 
@@ -27,29 +28,37 @@ operations = {
 }
 
 
-num1 = int(input("What's the first number?: "))
+def calculator():
+    num1 = int(input("What's the first number?: "))
 
-for op in operations:
-    print(op)
+    for op in operations:
+        print(op)
 
-should_continue = True
+    should_continue = True
 
-while should_continue:
-    operation_symbol = input("Pick an operation: ")
+    while should_continue:
+        operation_symbol = input("Pick an operation: ")
 
-    num2 = int(input("What's the next number?: "))
+        num2 = int(input("What's the next number?: "))
 
-    calculation_function = operations[operation_symbol]
-    answer = calculation_function(num1, num2)
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
 
-    print(f"{num1} {operation_symbol} {num2} = {answer}")
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-    loop = input(f"Type 'y' to continue calculating with {answer}, or 'n' to quit: ")
-    if loop == 'y':
-        num1 = answer
-    elif loop == 'n':
-        should_continue = False
-        print("Goodbye!")
-    else:
-        should_continue = False
-        print("Invalid input. Terminating program.")
+        loop = input(
+            f"Type 'y' to continue calculating with {answer}, 'n' to start a new calculation, or 'q' to quit: ").lower()
+        if loop == 'y':
+            num1 = answer
+        elif loop == 'n':
+            should_continue = False
+            calculator()
+        elif loop == 'q':
+            should_continue = False
+            print("Goodbye!")
+        else:
+            should_continue = False
+            print("Invalid input. Terminating program.")
+
+
+calculator()
